bcf630d643b669773b9364e2eb20dd53f408bbb7
Adish Sthalekar <asthalekar55@gmail.com>
2020-07-26 12:33:35 +0530

    added serial, deleted 2 delay

--- Blink.ino
+++ Blink.ino
@@ -26,6 +26,7 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  serial.begin(9600);
 }
 
 // the loop function runs over and over again forever
@@ -36,6 +37,5 @@ void loop() {
   delay(1000);                       // wait for a second
   delay(1000);  
   delay(1000);  
-  delay(1000);  
-  delay(1000);  
+ 
 }
